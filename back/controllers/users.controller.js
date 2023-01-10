189bf1e8ce59cc5d035529ef8170b3413edc7521
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-01-10 13:29:30 +0100

    creation route search/:keyword renvoi les user contenant l'input de la requete peu importe l'emplacement, recherche une occurence avec PSEUDO occurence avec mail, firstname et lastname peut etre à ajouter pour compléter la recherche annuaire

--- back/controllers/users.controller.js
+++ back/controllers/users.controller.js
@@ -15,8 +15,20 @@ const bcrypt = require("bcryptjs");
  */
 const usersController = {
 
+    searchUsersLikeKeyword: async (req, res, next) => {
+        try {
+            const keyword = req.params.keyword;
+            const [rows, fields] = await pool.query(`SELECT * FROM users WHERE pseudo LIKE '%${keyword}%'`)
+            res.json({
+                data: rows
+            })
+            pool.end()
+        } catch (error) {
+            console.log(error)
+            res.json({status: "Aucunes occurences trouvée en BDD avec le/les mot(s)-clef(s)."})
+        }
+    }, 
     
-
 
     getAll: async (req, res, next) => {
         try {
--- back/routes/users.routes.js
+++ back/routes/users.routes.js
@@ -1,13 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const router = express.Router()
+const router = express.Router();
 
-const userController = require("../controllers/users.controller")
+const userController = require("../controllers/users.controller");
 
-router.post('/register', userController.register)
+router.post('/register', userController.register);
 
-router.get('/list', userController.getAll)
+//route recherche par mot clef
+router.get('/search/:keyword', userController.searchUsersLikeKeyword);
+
+router.get('/list', userController.getAll);
 
 module.exports = router
 
