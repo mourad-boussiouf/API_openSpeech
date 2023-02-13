0b65eb10b1edbd27698c1656e8fdb698268a8784
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-02-13 16:33:07 +0100

    ajout drapeaux

--- front/screen/LanguageSelect.js
+++ front/screen/LanguageSelect.js
@@ -3,17 +3,25 @@ import { View, StyleSheet, Text, SafeAreaView } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import BackgroundDisplay from "../components/base/BackgroundDisplay";
 import IntroComponent from '../components/tutointroductif/IntroComponent';
+import '~flag-icons/sass/flag-icons.scss';
 
 
-
-export default function LanguageSelect(){
+const LanguageSelect = () => {
     return (
-        <View>
-            <BackgroundDisplay/> 
-            <IntroComponent/>
-        </View>
-    )
-}
+      <View style={styles.container}>
+        <Text>Select language Menu</Text>
+      </View>
+    );
+};
 
+export default LanguageSelect;
 
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   justifyContent: "center",
+   alignItems: "center",
+ },
+});
 
